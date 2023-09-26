6af6465b4c78245e41c629d988c6081be5412c79
ellleaa <144843448+ellleaa@users.noreply.github.com>
2023-09-26 20:11:08 +0200

    homework

---
+++ 03.py
@@ -0,0 +1,29 @@
+# NOTES
+#řetězce patří do třídy str
+#celá čísla do třídy int
+#čísla s desetinnou čárkou patří do třídy zvané float (floating point)
+#symboly: + - * ** / // %
+#chci zkusit:
+n = input("Please enter your name: ")
+print("Hello", n)
+
+str_seconds = input("Please enter the number of seconds you wish to convert")
+total_secs = int(str_seconds)
+
+hours = total_secs // 3600
+secs_still_remaining = total_secs % 3600
+minutes =  secs_still_remaining // 60
+secs_finally_remaining = secs_still_remaining  % 60
+
+print("Hrs=", hours, "mins=", minutes, "secs=", secs_finally_remaining)
+
+bruce = 5
+print(bruce)
+bruce = 7
+print(bruce)
+
+
+
+
+
+
